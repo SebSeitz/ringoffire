853dca8242dc26464af0c9daedc2533224983877
SebSeitz <sebastian_seitz13@web.de>
2022-01-19 00:30:52 +0100

    add player names and card animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartScreenComponent
+    StartScreenComponent,
+    GameComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
